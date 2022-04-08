4070c65bad89384d70bfc6f94f305d9de3248d11
Sean Stevens <sstevens88@gmail.com>
2022-04-07 22:19:58 -0700

    created Xcode project and skeleton + pods

---
+++ Spotify-Collab-Playlist-App/Pods/Target Support Files/Pods-Spotify-Collab-Playlist-App-Spotify-Collab-Playlist-AppUITests/Pods-Spotify-Collab-Playlist-App-Spotify-Collab-Playlist-AppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Spotify_Collab_Playlist_App_Spotify_Collab_Playlist_AppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Spotify_Collab_Playlist_App_Spotify_Collab_Playlist_AppUITests
+@end
---
+++ Spotify-Collab-Playlist-App/Pods/Target Support Files/Pods-Spotify-Collab-Playlist-AppTests/Pods-Spotify-Collab-Playlist-AppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Spotify_Collab_Playlist_AppTests : NSObject
+@end
+@implementation PodsDummy_Pods_Spotify_Collab_Playlist_AppTests
+@end
