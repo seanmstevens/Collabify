f7b4507a6bc8c9cc7b060e88600b509f177e5769
Xiqi Fei <violet.fei@hotmail.com>
2022-04-22 23:58:28 -0400

    profile

--- Spotify-Collab-Playlist-App/ProfileViewController.swift
+++ Spotify-Collab-Playlist-App/ProfileViewController.swift
@@ -6,16 +6,74 @@
 //
 
 import UIKit
+import AlamofireImage
+import Parse
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var userEmail: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        profileImage.layer.masksToBounds = true
+        profileImage.layer.cornerRadius = profileImage.bounds.width/2
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let user = PFUser.current()!
+        
+        let imageFile = user["profileImage"] as! PFFileObject
+        let url = URL(string: imageFile.url!)!
+        profileImage.af.setImage(withURL: url)
+        userName.text = user.username
+        userEmail.text = user.email
+//        userPassword.text = user.password
+        
+    }
 
+    @IBAction func onAddImageButton(_ sender: Any) {
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let image = info[.editedImage] as! UIImage
+        let size = CGSize(width: 160, height: 160)
+        let scaledImage = image.af.imageScaled(to: size)
+        
+        profileImage.image = scaledImage
+        
+        let imageData = profileImage.image!.pngData()
+        let file = PFFileObject(name: "image.png", data: imageData!)
+        
+        let user = PFUser.current()!
+        user["profileImage"] = file
+        
+        user.saveInBackground { (success, error) in
+            if success {
+                print("saved!")
+            } else {
+                print("error!")
+            }
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
@@ -25,5 +83,17 @@ class ProfileViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    
+    @IBAction func onLogoutButton(_ sender: Any) {
+        PFUser.logOut()
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else {return}
+        
+        delegate.window?.rootViewController = loginViewController
+        
+    }
+    
 }
